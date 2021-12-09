e33fbc971314d1ec78edfa31891c93634488ef9b
parking0930 <parking0930@naver.com>
2021-12-09 18:34:10 +0900

    Last Commit

--- FaceDetect.py
+++ FaceDetect.py
@@ -9,11 +9,12 @@ import telegram
 from os import listdir
 from os.path import isdir, isfile, join
 from datetime import datetime
+# 도어락 제어 GPIO, 14번 핀 사용 프로그램 실행시, 잠겨있는 상태가 Defalut가 되도록
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT, initial=GPIO.HIGH)
-bot = telegram.Bot(token='2063832595:AAHXOcez_70VveYrHl9c7xoRJuJ7SphoRAo')
+bot = telegram.Bot(token='2063832595:AAHXOcez_70VveYrHl9c7xoRJuJ7SphoRAo')  # 텔레그램 봇 연결 키
 chat_id = 2078275327
-# 얼굴 인식용 haar/cascade 로딩
+# 얼굴 인식용 haarcascade 로딩
 face_classifier = cv2.CascadeClassifier('./haarcascades/haarcascade_frontalface_default.xml')    
 
 # 사용자 얼굴 학습
@@ -24,6 +25,7 @@ def train(name):
     
     Training_Data, Labels = [], []
     
+    # 사용자의 사진을 불러옴
     for i, files in enumerate(face_pics):
         image_path = data_path + face_pics[i]
         images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -32,15 +34,17 @@ def train(name):
             continue    
         Training_Data.append(np.asarray(images, dtype=np.uint8))
         Labels.append(i)
+    # 학습할 데이터(이미지)가 없으면 함수 종료
     if len(Labels) == 0:
-        print("There is no data to train.")
+        print("학습할 이미지가 없습니다.")
         return None
+    # Lables 를 32비트 정수로 변환함
     Labels = np.asarray(Labels, dtype=np.int32)
     # 모델 생성
     model = cv2.face.LBPHFaceRecognizer_create()
     # 학습
     model.train(np.asarray(Training_Data), np.asarray(Labels))
-    print(name + " : Model Training Complete!!!!!")
+    print(name + " : 모델 학습 완료")
 
     #학습 모델 리턴
     return model
@@ -67,7 +71,7 @@ def trains():
         models[model] = result
 
     # 학습된 모델 딕셔너리 리턴
-    return models    
+    return models   
 
 #얼굴 검출
 def face_detector(img, size = 0.5):
@@ -92,8 +96,8 @@ def run(models):
             # 얼굴 검출 시도 
             image, face = face_detector(frame)
             try:            
-                min_score = 999       #가장 낮은 점수로 예측된 사람의 점수
-                min_score_name = ""   #가장 높은 점수로 예측된 사람의 이름
+                min_score = 999       #예측된 사람일 확률
+                min_score_name = ""   #예측된 사람의 이름
             
                 #검출된 사진을 흑백으로 변환 
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
@@ -101,23 +105,28 @@ def run(models):
                 #위에서 학습한 모델로 예측시도
                 for key, model in models.items():
                     result = model.predict(face)                
+                    #result[1] 에 들어가는 값은 신뢰도 임
                     if min_score > result[1]:
                         min_score = result[1]
                         min_score_name = key
                     
-                #min_score 신뢰도이고 0에 가까울수록 자신과 같다는 뜻이다.         
+                # 0에 가까울 수록 정확하다.         
                 if min_score < 500:
-                    #????? 어쨋든 0~100표시하려고 한듯 
+                    # 정확도 
                     confidence = int(100*(1-(min_score)/300))
                     # 유사도 화면에 표시 
                     display_string = str(confidence)+'% Confidence it is ' + min_score_name
                 cv2.putText(image,display_string,(100,120), cv2.FONT_HERSHEY_COMPLEX,1,(250,120,255),2)
-                #75 보다 크면 동일 인물로 간주해 UnLocked! 
-                if confidence > 70:
+                
+                #80 보다 크면 동일 인물로 간주해 UnLocked! 
+                if confidence > 80:
                     cv2.putText(image, "Unlocked : " + min_score_name, (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
                     cv2.imshow('Face Cropper', image)
+                    
+                    # 저장할 파일의 경로와 이름지정
                     now = str(datetime.now().strftime('%Y-%m-%d %H:%M'))
                     file_list = os.listdir('./Save')
+                    # 중복된 사진이 있는지 검사용 변수
                     exist_person = False
                     for filename in file_list:
                         if filename == (min_score_name+'|'+now+'.jpg'):
@@ -126,25 +135,47 @@ def run(models):
                             break
                     if exist_person:
                         continue
-                
+                    # 이름_사원번호를 _ 기준으로 분리
+                    p_name = min_score_name.split('_')[0]
+                    uid = min_score_name.split('_')[1]
+                    # 인증된 사용자이므로 도어락 열기
                     GPIO.output(14, GPIO.LOW)
-                    #텔레그램으로 사진 전송
+                    #텔레그램으로 사진 전송 (인증된 사용자)
                     cap_name = min_score_name+'|'+now + ".jpg"
                     cv2.imwrite('./Save/'+cap_name, frame)
                     bot.sendPhoto(chat_id=chat_id,photo=open('./Save/'+cap_name,'rb'))
-                    bot.send_message(chat_id=chat_id, text="") 
+                    log_text = '사원번호:'+uid+'\n이름:'+p_name
+                    bot.send_message(chat_id=chat_id, text=log_text) 
+                
+                #80 이하면 타인.. Locked!!!
                 else:
-                #75 이하면 타인.. Locked!!! 
+                    for filename in file_list:
+                        if filename == ('X|'+now+'.jpg'):
+                            print("이미 있는 사진")
+                            exist_person = True
+                            break
+                    if exist_person:
+                        continue
+                    #텔레그램으로 사진 전송 (비인가 사용자)
+                    cap_name = 'X|'+now + ".jpg"
+                    cv2.imwrite('./Save/'+cap_name, frame)
+                    bot.sendPhoto(chat_id=chat_id,photo=open('./Save/'+cap_name,'rb'))
+                    log_text = '인가되지 않은 사용자 발견!'
+                    bot.send_message(chat_id=chat_id, text=log_text) 
                     cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
                     cv2.imshow('Face Cropper', image)
+
+            #얼굴 검출 안됨 
             except:
-                #얼굴 검출 안됨 
                 cv2.putText(image, "Face Not Found", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
                 cv2.imshow('Face Cropper', image)
                 pass
+            #Enter 입력으로 프로그램 종료
             if cv2.waitKey(1)==13:
                 break
+        #카메라 끄기
         cap.release()
+        #열려있는 창 닫기
         cv2.destroyAllWindows()
     except KeyboardInterrupt:
         GPIO.cleanup()
@@ -154,5 +185,5 @@ if __name__ == "__main__":
     GPIO.output(14, GPIO.HIGH)
     # 학습 시작
     models = trains()
-    # 고!
+    # 시작
     run(models)
--- FaceRegister.py
+++ FaceRegister.py
@@ -5,25 +5,26 @@ import json
 from os import makedirs
 from os.path import isdir
 
-# 얼굴 저장 함수
+# 얼굴을 저장할 디렉토리
 face_dirs = 'faces/'
-
-face_classifier = cv2.CascadeClassifier('/home/pi/haarcascades/haarcascade_frontalface_default.xml')
+# haarcascade 학습모듈 로드
+face_classifier = cv2.CascadeClassifier('/home/pi/pj/haarcascades/haarcascade_frontalface_default.xml')
 
 # 얼굴 검출 함수
 def face_extractor(img):
+    # 사진을 회색으로 변환 (연산량을 줄일 수 있음)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
-    # 얼굴이 없으면 패스!
+    # 얼굴이 없으면 넘어감
     if faces is():
         return None
     # 얼굴이 있으면 얼굴 부위만 이미지로 만들고
     for(x,y,w,h) in faces:
         cropped_face = img[y:y+h, x:x+w]
-    # 리턴!
+    # 자른 이미지를 반환
     return cropped_face
 
-# 얼굴만 저장하는 함수
+# 사진 촬영 함수
 def take_pictures(name):
     # 해당 이름의 폴더가 없다면 생성
     if not isdir(face_dirs+name):
@@ -38,44 +39,42 @@ def take_pictures(name):
         ret, frame = cap.read()
         # 사진에서 얼굴 검출 , 얼굴이 검출되었다면 
         if face_extractor(frame) is not None:
-            
             count+=1
-            # 200 x 200 사이즈로 줄이거나 늘린다음
+            # 촬영한 사진을 200 x 200 사이즈로 조정
             face = cv2.resize(face_extractor(frame),(200,200))
             # 흑백으로 바꿈
-            # face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
             # 200x200 흑백 사진을 faces/얼굴 이름/userxx.jpg 로 저장
-            file_name_path = face_dirs + name + '/user'+str(count)+'.jpg'
+            file_name_path = face_dirs + name + '/' + str(count) +'.jpg'
             cv2.imwrite(file_name_path,face)
-
+            # 촬영된 사진이 몇번째 사진인지 창 안에 표시
             cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            cv2.imshow('Face Cropper',face)
+            cv2.imshow('Detected Face',face)
         else:
             print("Face not Found")
             pass
         
-        # 얼굴 사진 100장을 다 얻었거나 enter키 누르면 종료
-        if cv2.waitKey(1)==13 or count==10:
+        # 얼굴 사진 30장을 촬영했거나 enter키 누르면 종료
+        if cv2.waitKey(1)==13 or count==30:
             break
 
+    # 촬영 종료
     cap.release()
+    # 창 닫음
     cv2.destroyAllWindows()
-    print('Colleting Samples Complete!!!')
-
+    print('학습 샘플이미지 추출 완료')
 
 if __name__ == "__main__":
     p_number = ""
     # 사진 저장할 이름을 넣어서 함수 호출
     while True:
         p_number = input("사원번호 : ")
-        request_data = requests.get("http://192.168.0.61:8081/infoserver/selectAuthorized?id="+p_number)
-        # print(request_data.text)
+        request_data = requests.get("http://172.20.10.5:8081/infoserver/selectAuthorized?id="+p_number)
         info = json.loads(request_data.text)
-        # print(info["authorized"])
         if info["authorized"] == None:
             break
         print("이미 존재하는 사원번호입니다.")
+    # print(result)
 
     name = input("학습할 사람의 이름 : ")
     position = input("직책 : ")
@@ -85,5 +84,4 @@ if __name__ == "__main__":
                 "name" : name,
                 "position": position
                 }
-    result = requests.post("http://192.168.0.61:8081/infoserver/register", data=response_data)
-    # print(result)
+    result = requests.post("http://172.20.10.5:8081/infoserver/register", data=response_data)
